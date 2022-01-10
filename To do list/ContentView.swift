40623b350b7ca5c873c1b554887590da8037c186
Victor <victor>
2022-01-10 10:41:22 -0600

    Initial Commit

---
+++ To do list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To do list
+//
+//  Created by Student on 1/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To do list
+//
+//  Created by Student on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
